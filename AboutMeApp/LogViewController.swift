c28416bafd950fe651b1de28c883a133df8c2069
Andrey <Rhymes.un.limit@gmail.com>
2025-05-10 21:31:19 +0300

    add element VC

--- AboutMeApp/LogViewController.swift
+++ AboutMeApp/LogViewController.swift
@@ -1,12 +1,11 @@
 import UIKit
 
-class ViewController: UIViewController {
+class LogViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    
 
 }
-
