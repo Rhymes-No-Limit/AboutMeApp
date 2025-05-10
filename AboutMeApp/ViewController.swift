66dc445e5f75e6ca0d45bb90cb1155b9c17e2784
Andrey <Rhymes.un.limit@gmail.com>
2025-05-10 20:59:10 +0300

    prepare project

--- AboutMeApp/ViewController.swift
+++ AboutMeApp/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  AboutMeApp
-//
-//  Created by Андрей Терентьев on 10.05.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
