a35c15daf5f8fb6b48a403c6a7c690ac05f90f38
Andrey <Rhymes.un.limit@gmail.com>
2025-05-11 18:26:24 +0300

    add functionality show alert in forgets

--- AboutMeApp/MainViewController.swift
+++ AboutMeApp/MainViewController.swift
@@ -2,11 +2,43 @@ import UIKit
 
 final class MainViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    @IBOutlet var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        logInButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func logInAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func forgotUserNameAction() {
+        let alert = UIAlertController(
+            title: "User name:",
+            message: "Your user name: Rhymes",
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+    
+    @IBAction func forgotPassordAction() {
+        let alert = UIAlertController(
+            title: "Password:",
+            message: "Your password: 123",
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+    
 }
 
