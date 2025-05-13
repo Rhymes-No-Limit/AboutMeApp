117d7dd654aaf9ce8b524a02435bf9c0b611a1fd
Andrey <Rhymes.un.limit@gmail.com>
2025-05-13 14:42:52 +0300

    optimization main view controller

--- AboutMeApp/MainViewController.swift
+++ AboutMeApp/MainViewController.swift
@@ -6,48 +6,61 @@ final class MainViewController: UIViewController {
     @IBOutlet var passwordTF: UITextField!
     @IBOutlet var logInButton: UIButton!
     
+    private let user = "Rhymes"
+    private let password = "123"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupKeyboardHandling()
+
         logInButton.layer.cornerRadius = 10
         passwordTF.isSecureTextEntry = true
     }
-
-    @IBAction func logInAction(_ sender: UIButton) {
-        let currentLogin = "Rhymes"
-        let currentPassword = "123"
-        
-        guard let loginText = userNameTF.text, !loginText.isEmpty else {
-            return showAlert(withTitle: "Error", andMessage: "Input login or password")
-        }
-        
-        guard let passwordText = passwordTF.text, !passwordText.isEmpty else {
-            return showAlert(withTitle: "Error", andMessage: "Input login or password")
-        }
-        
-        if loginText == currentLogin && passwordText == currentPassword {
-            performSegue(withIdentifier: "showLogScreen", sender: self)
-        } else {
-            showAlert(withTitle: "Error", andMessage: "Wrong current login or password") {
-                self.passwordTF.text = ""
-            }
-        }
-        
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? LogViewController else { return }
+        welcomeVC.userName = user
     }
     
-
-    
-    @IBAction func forgotUserNameAction() {
-        showAlert(withTitle: "User name:", andMessage: "Your user name: Rhymes")
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
-    @IBAction func forgotPassordAction() {
-        showAlert(withTitle: "Password:", andMessage: "Your password: 123")
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard let loginText = userNameTF.text,
+              let passwordText = passwordTF.text,
+              !loginText.isEmpty,
+              !passwordText.isEmpty else {
+                showAlert(withTitle: "Error", andMessage: "Input login or password")
+            return false
+        }
+        
+        guard userNameTF.text == user, passwordTF.text == password else {
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login and password") {
+                    self.passwordTF.text = ""
+                }
+            return false
+        }
+        return true
     }
     
+    
+    
+    @IBAction func forgotRegisterData(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(withTitle: "User name:", andMessage: "Your user name: \(user)")
+        : showAlert(withTitle: "Password:", andMessage: "Your password: \(password)")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
     private func showAlert(withTitle title: String,
                            andMessage message: String,
-                           completion: (() -> ())? = nil) {
+                           completion: (() -> Void)? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
             completion?()
@@ -56,44 +69,4 @@ final class MainViewController: UIViewController {
         present(alert, animated: true)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            let destinationVC = segue.destination as? LogViewController
-            destinationVC?.userName = userNameTF.text
-        
-        userNameTF.text = ""
-        passwordTF.text = ""
-    }
-    
-    
 }
-
-extension UIViewController {
-    func setupKeyboardHandling() {
-            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-            view.addGestureRecognizer(tapGesture)
-
-            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow),
-                                                   name: UIResponder.keyboardWillShowNotification, object: nil)
-            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
-                                                   name: UIResponder.keyboardWillHideNotification, object: nil)
-        }
-
-        @objc private func dismissKeyboard() {
-            view.endEditing(true)
-        }
-
-        @objc private func keyboardWillShow(notification: Notification) {
-            guard view.frame.origin.y == 0 else { return }
-
-            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-                view.frame.origin.y -= keyboardSize.height / 2
-            }
-        }
-
-        @objc private func keyboardWillHide(notification: Notification) {
-            if view.frame.origin.y != 0 {
-                view.frame.origin.y = 0
-            }
-        }
-}
-
