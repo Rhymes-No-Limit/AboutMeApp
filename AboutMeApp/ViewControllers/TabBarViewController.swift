afd6e91688c80b3f46306f84054e14ccdcc709a5
Andrey <Rhymes.un.limit@gmail.com>
2025-05-26 00:21:23 +0300

    creating color tab bar

--- AboutMeApp/ViewControllers/TabBarViewController.swift
+++ AboutMeApp/ViewControllers/TabBarViewController.swift
@@ -1,29 +1,15 @@
-//
-//  TabBarViewController.swift
-//  AboutMeApp
-//
-//  Created by Андрей Терентьев on 26.05.2025.
-//
-
 import UIKit
 
 class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
